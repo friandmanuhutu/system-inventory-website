434bfb8a97289c5f3ca1c91a6ad4c7a1c2132dfe
7Riv <riv74.sh@gmail.com>
2025-08-07 09:57:03 +0700

    [bootstrap]

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,7 @@
 import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import * as bootstrap from 'bootstrap';
+
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
